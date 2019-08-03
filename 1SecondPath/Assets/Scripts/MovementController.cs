b4b5af3fa69c1a7583e34a12ccb81b4852291112
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-03 14:20:56 +0200

    Maze
    Added 3d maze
    Working in collisions

---
+++ 1SecondPath/Assets/Scripts/MovementController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Rigidbody rb;
+    float velocity = 1f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector3(0f, 0f,0f);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (rb.velocity.y < 1.5)
+            {
+                rb.velocity += new Vector3(0f, velocity * Time.timeScale, 0f);
+            }
+           
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (rb.velocity.y > -1.5)
+            {
+                rb.velocity -= new Vector3(0f, velocity * Time.timeScale, 0f);
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (rb.velocity.x > -1.5)
+            {
+                rb.velocity -= new Vector3(velocity * Time.timeScale, 0f, 0f);
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (rb.velocity.x < 1.5)
+            {
+                rb.velocity += new Vector3(velocity * Time.timeScale, 0f, 0f);
+            }
+
+        }
+    }
+}
