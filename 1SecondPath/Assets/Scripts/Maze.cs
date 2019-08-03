354a72006b44b43bcc69397b06d362f1b74ee740
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-03 19:00:21 +0200

    Maze walls
    Added walls to doors which are not active

--- 1SecondPath/Assets/Scripts/Maze.cs
+++ 1SecondPath/Assets/Scripts/Maze.cs
@@ -9,6 +9,8 @@ public class Maze : MonoBehaviour
     [SerializeField]
     GameObject []Doors;
     [SerializeField]
+    GameObject[] doorWalls;
+    [SerializeField]
     GameObject mesh;
 
     GameObject player;
@@ -23,42 +25,50 @@ public class Maze : MonoBehaviour
         timer = 0f;
         rnd = new System.Random();
         enter = rnd.Next(0, Doors.Length);
+        Debug.Log("Enter: "+enter);
 
         do
         {
-            exit = rnd.Next(0, Doors.Length);
-        } while (exit != enter);
-
+            exit = rnd.Next(0, Doors.Length);           
+        } while (exit == enter);
+        Debug.Log("Exit: "+exit);
         switch (enter)
         {
             case 0:
                 transform.Rotate(0f, 0f, 0f);      
-                Debug.Log("Door 1");
                 break;              
-
             case 1:
                 transform.Rotate(0f, 0f, 90f);
-                Debug.Log("Door 2");
                 break;
             case 2:
                 transform.Rotate(0f, 0f, 180f);        
-                Debug.Log("Door 3");
                 break;
             case 3:
                 transform.Rotate(0f, 0f, 270f);
-                Debug.Log("Door 4");
                 break;
         }
-        player.transform.position = new Vector3(Doors[enter].transform.position.x,Doors[enter].transform.position.y,-0.8f);
+
+        Doors[exit].GetComponent<BoxCollider>().enabled = false;
+        doorWalls[enter].SetActive(false);
+        doorWalls[exit].SetActive(false);
+
+        player.transform.position = new Vector3(Doors[enter].transform.position.x,Doors[enter].transform.position.y+0.5f,-0.8f);
     }
 
     
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= 3)
+        if (timer >= 2)
         {
             mesh.SetActive(false);
+            for (int i = 0; i < doorWalls.Length; ++i)
+            {
+                if (i != enter && i != exit)
+                {
+                    doorWalls[i].GetComponent<MeshRenderer>().enabled = false;
+                }
+            }
         }
     }
 }
