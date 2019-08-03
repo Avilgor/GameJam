ccccc0a880ef917b286099f6eb346b9d90d00296
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-03 13:45:16 +0200

    Maze selector
    Added script to select  maze at start

---
+++ 1SecondPath/Assets/Scripts/Maze.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Maze : MonoBehaviour
+{
+    [SerializeField]
+    GameObject []Doors;
+
+    private System.Random rnd;
+
+    void Start()
+    {
+        rnd = new System.Random();
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ 1SecondPath/Assets/Scripts/MazeSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class MazeSelector : MonoBehaviour
+{
+    [SerializeField]
+    GameObject []maze;
+
+    private System.Random rnd;
+
+    void Start()
+    {
+        rnd = new System.Random();
+        GameObject chosen = Instantiate(maze[rnd.Next(0, maze.Length)]);
+    }
+}
